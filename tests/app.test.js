0677e288f6ee6e831698a0dbb45ffe36ff8c6581
Tatuolkinuora <ag3462@student.jamk.fi>
2025-11-20 14:05:10 +0200

    Test enviroment built

---
+++ tests/app.test.js
@@ -0,0 +1,8 @@
+import { describe, test, expect, it } from 'vitest';
+import  from '/public/app.js';
+
+describe('Taskin luonnin testaus', () => {
+  it('should do stuff', () => {});
+
+  test('should do other stuff', () => {});
+});
